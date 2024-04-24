4318b6464f5f0bebc2515811501a702ba9702be4
raula09 <163653632+raula09@users.noreply.github.com>
2024-04-24 16:38:39 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Retrieve the stored username from local storage
+let username = localStorage.getItem('username');
+
+
+if (!username) {
+  let userInput;
+  
+ 
+  while (!userInput) {
+    userInput = prompt('Please enter your username:');
+  }
+  
+  
+  localStorage.setItem('username', userInput);
+  
+ 
+  username = userInput;
+}
+
+
+document.querySelector('.guest1').textContent = username;
